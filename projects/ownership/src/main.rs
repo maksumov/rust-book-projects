bc0f7a5fc4b132340c6e1d132db4c4b45927c374
Marat Maksumov <maksumov@users.noreply.github.com>
2026-08-30 00:20:32 +0500

    ownership: calling first_word with different slice forms

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
-    let s = String::from("hello world");
+    let my_string = String::from("hello world");
 
-    let word = first_word(&s);
+    // `first_word` works on slices of `String`s, whether partial or whole.
+    let _word = first_word(&my_string[0..6]);
+    let _word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s.
+    let _word = first_word(&my_string);
 
-    println!("the first word is: {word}");
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or
+    // whole.
+    let _word = first_word(&my_string_literal[0..6]);
+    let _word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let _word = first_word(my_string_literal);
 }
 
 fn first_word(s: &str) -> &str {
