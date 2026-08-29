4a8274de5b305ac9b814d5e689d4b1dcfcc5e44a
Marat Maksumov <maksumov@users.noreply.github.com>
2026-08-29 20:11:07 +0500

    ownership: ownership and functions (move into function params)

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,12 +1,24 @@
 fn main() {
-    let x = 5;
-    let y = x;
+    let s = String::from("hello"); // s comes into scope
 
-    println!("{x}");
-    println!("{y}");
+    takes_ownership(s); // s's value moves into the function...
+    // ... and so is no longer valid here
 
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    let x = 5; // x comes into scope
 
-    println!("s1 = {s1}, s2 = {s2}");
-}
+    makes_copy(x); // Because i32 implements the Copy trait,
+    // x does NOT move into the function,
+    // so it's okay to use x afterward.
+} // Here, x goes out of scope, then s. However, because s's value was moved,
+// nothing special happens.
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{some_string}");
+} // Here, some_string goes out of scope and `drop` is called. The backing
+// memory is freed.
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{some_integer}");
+} // Here, some_integer goes out of scope. Nothing special happens.
