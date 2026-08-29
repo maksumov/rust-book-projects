2bb12348d4aaa32127850b82fd92e50177ca331e
Marat Maksumov <maksumov@users.noreply.github.com>
2026-08-29 15:52:36 +0500

    branches: if condition must be bool (intentionally non-compiling)

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
     let number = 3;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    if number {
+        println!("number was three");
     }
 }
