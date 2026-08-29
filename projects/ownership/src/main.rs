263b5f9f6f740f23a1f9f99d6233569d734b5d66
Marat Maksumov <maksumov@users.noreply.github.com>
2026-08-29 18:58:36 +0500

    ownership: mutable String with push_str

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    // s is not valid here, since it's not yet declared
-    let s = "hello"; // s is valid from this point forward
+    let mut s = String::from("hello");
 
-    // do stuff with s
-} // this scope is now over, and s is no longer valid
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{s}"); // this will print `hello, world!`
+}
