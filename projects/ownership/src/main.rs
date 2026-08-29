213b76cce607157390593cb1b7d6b7f22a550f2a
Marat Maksumov <maksumov@users.noreply.github.com>
2026-08-29 21:46:14 +0500

    ownership: fix immutable/mutable conflict via non-lexical lifetimes

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -3,7 +3,9 @@ fn main() {
 
     let r1 = &s; // no problem
     let r2 = &s; // no problem
-    let r3 = &mut s; // BIG PROBLEM
+    println!("{r1} and {r2}");
+    // Variables r1 and r2 will not be used after this point.
 
-    println!("{r1}, {r2}, and {r3}");
+    let r3 = &mut s; // no problem
+    println!("{r3}");
 }
