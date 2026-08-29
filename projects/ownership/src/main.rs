5f966244f57e794f651e80e5d7691c54787ae2cf
Marat Maksumov <maksumov@users.noreply.github.com>
2026-08-29 20:55:30 +0500

    ownership: only one mutable reference at a time (intentionally non-compiling)

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let mut s = String::from("hello");
 
-    change(&mut s);
+    let r1 = &mut s;
+    let r2 = &mut s;
 
-    println!("{s}");
+    println!("{r1}, {r2}");
 }
 
 fn change(some_string: &mut String) {
