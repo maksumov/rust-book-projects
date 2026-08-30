6d64caf360b31978fcd0601fb6cbc3a7bab1ebfd
Marat Maksumov <maksumov@users.noreply.github.com>
2026-08-30 13:10:15 +0500

    structs: tuple structs (Color, Point)

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -1,33 +1,13 @@
 #[allow(dead_code)]
 #[derive(Debug)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+struct Color(i32, i32, i32);
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Point(i32, i32, i32);
 
 fn main() {
-    let mut user1 = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123"),
-    );
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 
-    user1.email = String::from("anotheremail@example.com");
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-
-    println!("{user2:?}");
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
+    println!("{black:?}\n{origin:?}");
 }
