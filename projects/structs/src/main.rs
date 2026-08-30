f14757a702c83dcf412e4504485b73347ba60b13
Marat Maksumov <maksumov@users.noreply.github.com>
2026-08-30 12:15:32 +0500

    structs: mutating struct instance fields

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -6,10 +6,12 @@ struct User {
 }
 
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         active: true,
         username: String::from("someusername123"),
         email: String::from("someone@example.com"),
         sign_in_count: 1,
     };
+
+    user1.email = String::from("anotheremail@example.com");
 }
