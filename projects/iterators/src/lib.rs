265fc33e3772c797c05bca9fa820340a42407bc8
Marat Maksumov <maksumov@users.noreply.github.com>
2026-09-10 02:56:53 +0500

    iterators: sum consuming adapter test (listing 13-13)

--- projects/iterators/src/lib.rs
+++ projects/iterators/src/lib.rs
@@ -32,4 +32,20 @@ mod tests {
         assert_eq!(v1_iter.next(), Some(&3));
         assert_eq!(v1_iter.next(), None);
     }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        // We aren't allowed to use v1_iter after the call to sum,
+        // because sum takes ownership of the iterator we call it on:
+        // println!("{v1_iter:?}");
+        // error[E0382]: borrow of moved value: `v1_iter`
+
+        assert_eq!(total, 6);
+    }
 }
